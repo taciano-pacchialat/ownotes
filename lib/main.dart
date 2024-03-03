c74c81adc8bc190ff4465756c7f2eddb9e10dc30
Taciano <taciano3003@gmail.com>
2024-03-03 13:04:55 -0300

    editing existing notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ownotes/constants/routes.dart';
 import 'package:ownotes/services/auth/auth_service.dart';
 import 'package:ownotes/views/login_view.dart';
-import 'package:ownotes/views/notes/new_notes_view.dart';
+import 'package:ownotes/views/notes/create_update_note_view.dart';
 import 'package:ownotes/views/notes/notes_view.dart';
 import 'package:ownotes/views/register_view.dart';
 import 'package:ownotes/views/verify_email_view.dart';
@@ -22,7 +22,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
